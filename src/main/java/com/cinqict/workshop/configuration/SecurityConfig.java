81ebb8b9a6edb1e2a2b2739d96fb4314781106cb
Ian van Nieuwkoop <ian.van.nieuwkoop@rws.nl>
2023-05-18 18:56:40 +0200

    002: lamda dsl

--- src/main/java/com/cinqict/workshop/configuration/SecurityConfig.java
+++ src/main/java/com/cinqict/workshop/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cinqict.workshop.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,9 +23,9 @@ public class SecurityConfig {
                           // .requestMatchers("/error/**").permitAll()
                            .anyRequest().denyAll();
                })
-               .formLogin()
-               .and().httpBasic()
-               .and().build();
+               .formLogin(Customizer.withDefaults())
+               .httpBasic(Customizer.withDefaults())
+               .build();
     }
 }
 
