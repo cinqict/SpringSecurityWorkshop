6539c27e2382b58876efca9cfdec22c001994ff0
Ian van Nieuwkoop <ian.van.nieuwkoop@rws.nl>
2023-05-24 13:33:21 +0200

    003: init

--- src/main/java/com/cinqict/workshop/WorkshopApplication.java
+++ src/main/java/com/cinqict/workshop/WorkshopApplication.java
@@ -2,6 +2,7 @@ package com.cinqict.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class WorkshopApplication {
@@ -9,5 +10,4 @@ public class WorkshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopApplication.class, args);
 	}
-
 }
--- src/main/java/com/cinqict/workshop/configuration/SecurityConfig.java
+++ src/main/java/com/cinqict/workshop/configuration/SecurityConfig.java
@@ -5,6 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,9 +32,38 @@ public class SecurityConfig {
                           // .requestMatchers("/error/**").permitAll()
                            .anyRequest().denyAll();
                })
-               .formLogin(Customizer.withDefaults())
+               //.formLogin(Customizer.withDefaults())
                .httpBasic(Customizer.withDefaults())
                .build();
+    }
+
+//    @Bean
+//    PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        //default encoding is Bcrypt
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+
+        UserDetails user = User.builder()
+                .username("spring")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user1 = User.builder()
+                .username("user")
+                .password("{ldap}"+new LdapShaPasswordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, user1);
     }
 }
 
